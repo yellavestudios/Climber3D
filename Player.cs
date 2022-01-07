d1a7bf5740cfcdfe5b7b5ef7286f508afc3bc916
Christopher Pearl <53843747+yellavestudios@users.noreply.github.com>
2022-01-07 15:19:15 -0800

    Add files via upload

--- Player.cs
+++ Player.cs
@@ -11,6 +11,9 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _speed = 4.5f;
     private Vector3 _playerVelocity;
+    [SerializeField]
+    private float _jumpheight = 2.0f;
+    private float _gravity = -9.81f;
 
 
 
@@ -25,21 +28,34 @@ public class Player : MonoBehaviour
     void Update()
     {
         //get horizontal input
-        //define direction based on that input
+        
         _groundedPlayer = _controller.isGrounded;
         if (_groundedPlayer && _playerVelocity.y < 0)
         {
             _playerVelocity.y = 0f;
         }
 
-
+        //define direction based on that input
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        //Move based on that direction
         _controller.Move(move * Time.deltaTime * _speed);
 
         if (move != Vector3.zero)
         {
             gameObject.transform.forward = move;
         }
-        //Move based on that direction
+
+        //allow player to jump
+        if(Input.GetKeyDown(KeyCode.Space) && _groundedPlayer)
+        {
+            _playerVelocity.y += Mathf.Sqrt(_jumpheight * -2.0f * _gravity );
+
+        }
+
+        _playerVelocity.y += _gravity * Time.deltaTime;
+        _controller.Move(_playerVelocity * Time.deltaTime);
+
+
+       
     }
 }
